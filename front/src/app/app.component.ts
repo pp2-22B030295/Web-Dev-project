a2a418fdebab0bb915f1566f3840ac4cc2e82c2c
22B030295-KBTU <aisultan200522@mail.ru>
2024-04-18 01:41:13 +0600

    Header first commit

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {ProfileComponent} from "./Component/profile/profile.component";
 import {ProfileReviewsComponent} from "./Component/profile-reviews/profile-reviews.component";
 import {ProfileLibraryComponent} from "./Component/profile-library/profile-library.component";
@@ -13,4 +13,20 @@ import {ProfileLibraryComponent} from "./Component/profile-library/profile-libra
 })
 export class AppComponent {
   title = 'front';
+  constructor(private router: Router) {}
+  navigateToProfileInfo() {
+    this.router.navigate(['profile']);
+  }
+  navigateToProfileReviews() {
+    this.router.navigate(['profile/reviews']);
+  }
+  navigateToProfileLibrary() {
+    this.router.navigate(['profile/library']);
+  }
+  navigateToLogin() {
+    this.router.navigate(['login']);
+  }
+  navigateToSignIn() {
+    this.router.navigate(['sign_in']);
+  }
 }
